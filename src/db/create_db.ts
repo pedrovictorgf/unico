515286814d82a5bb05eccaf7cfaeec6bf7cda03e
Pedro Victor Guerra de Figueiredo <pedrovictorgf@gmail.com>
2021-07-05 17:10:19 -0300

    changed created_db to only run locally

--- src/db/create_db.ts
+++ src/db/create_db.ts
@@ -1,41 +1,32 @@
 import knex from 'knex';
 import config from '../knexfile'
 
+if(process.env['NODE_ENV'] !== 'production') {
+	const conn = knex({
+		client: "pg",
+    	connection: {
+			host:'db',
+			user:'postgres',
+			password:'postgres',
+			database:'postgres'
+		}
+ 	})
 
-const conn = process.env['NODE_ENV'] === 'production' ? 
-knex({
-	client: "pg",
-	connection: {
-		host:'ec2-3-224-7-166.compute-1.amazonaws.com',
-		user:'ejkhwpdyxyxoym',
-		password:'83198170fac71cac70a022c4e83ee7c5d5acabec39dd38df9bcace21f87f7d84',
-		database:'dc058oog11cse2'
-	}
-}):
-knex({
-	client: "pg",
-    connection: {
-		host:'db',
-		user:'postgres',
-		password:'postgres',
-		database:'postgres'
-	}
-})
+	let dbsWeShouldHave = ['unico'];
 
-let dbsWeShouldHave = ['unico'];
+	(async () => {
+		let dbs = await conn('pg_database').select('datname');
 
-(async () => {
-	let dbs = await conn('pg_database').select('datname');
+		dbs = dbs.map(item => item.datname);
 
-	dbs = dbs.map(item => item.datname);
+		dbsWeShouldHave = dbsWeShouldHave.filter(item => {
+			return !dbs.includes(item)
+		});
 
-	dbsWeShouldHave = dbsWeShouldHave.filter(item => {
-		return !dbs.includes(item)
-	});
+		for (const db of dbsWeShouldHave) {
+			await conn.raw(`create database ${db}`)
+		}
 
-	for (const db of dbsWeShouldHave) {
-		await conn.raw(`create database ${db}`)
-	}
-
-	conn.destroy();
-})();
+		conn.destroy();
+	})();
+}
